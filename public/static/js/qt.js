eebfc899f826b09d4b84085d8349a5a338cefb6f
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-14 09:49:58 -0700

    combined constructors into size and position
    To reduce visual clutter I combined this.x and y into 'position'
    and sizex and sizey into 'size' with values width and height

--- public/static/js/qt.js
+++ public/static/js/qt.js
@@ -9,11 +9,9 @@
  */
 
 export default class Quadtree { 
-    constructor({ x = 0, y = 0, sizex = width, sizey = height, capacity = 10 } = {}) {
-        this.x = x;
-        this.y = y;
-        this.sizex = sizex;
-        this.sizey = sizey;
+    constructor({ position ={x : 0, y : 0}, size ={width : width, height : height}, capacity = 10 } = {}) {
+        this.position = position
+        this.size = size
         this.items = [];
         this.children = [];
         this.capacity = capacity;
@@ -26,31 +24,31 @@ export default class Quadtree {
 
     contains(item) {
         return (
-            item.x > this.x && item.x < this.x + this.sizex &&
-            item.y > this.y && item.y < this.y + this.sizey)
+            item.x > this.position.x && item.x < this.position.x + this.size.width &&
+            item.y > this.position.y && item.y < this.position.y + this.size.height)
     }
 
     intersects(range) {
-        const dx = abs(range.x - (this.x + this.sizex / 2)),
-            dy = abs(range.y - (this.y + this.sizey / 2)),
-            dcorner = sqrt(pow(this.sizex / 2, 2) + pow(this.sizey / 2, 2))
+        const dx = abs(range.x - (this.position.x + this.size.width / 2)),
+            dy = abs(range.y - (this.position.y + this.size.height / 2)),
+            dcorner = sqrt(pow(this.size.width / 2, 2) + pow(this.size.height / 2, 2))
 
         // no possible interection
-        if (dx > range.r + this.sizex / 2 || dy > range.r + this.sizey / 2) return false
+        if (dx > range.r + this.size.width / 2 || dy > range.r + this.size.height / 2) return false
 
         // corner exclusion
-        if (dist(range.x, range.y, this.x + this.sizex / 2, this.y + this.sizey / 2) > dcorner + range.r) return false
+        if (dist(range.x, range.y, this.position.x + this.size.width / 2, this.position.y + this.size.height / 2) > dcorner + range.r) return false
 
         return true
     }
 
     subdivide() {
-        const w = this.sizex / 2,
-            h = this.sizey / 2
-        this.children.push(new Quadtree({ x: this.x, y: this.y, sizex: w, sizey: h, capacity:this.capacity}));
-        this.children.push(new Quadtree({ x: this.x + w, y: this.y, sizex: w, sizey: h, capacity:this.capacity }));
-        this.children.push(new Quadtree({ x: this.x, y: this.y + h, sizex: w, sizey: h, capacity:this.capacity }));
-        this.children.push(new Quadtree({ x: this.x + w, y: this.y + h, sizex: w, sizey: h, capacity:this.capacity }));
+        const w = this.size.width / 2,
+            h = this.size.height / 2
+        this.children.push(new Quadtree({ x: this.position.x, y: this.position.y, sizex: w, sizey: h, capacity:this.capacity}));
+        this.children.push(new Quadtree({ x: this.position.x + w, y: this.position.y, sizex: w, sizey: h, capacity:this.capacity }));
+        this.children.push(new Quadtree({ x: this.position.x, y: this.position.y + h, sizex: w, sizey: h, capacity:this.capacity }));
+        this.children.push(new Quadtree({ x: this.position.x + w, y: this.position.y + h, sizex: w, sizey: h, capacity:this.capacity }));
         this.hasSplit = true
     }
 
@@ -81,7 +79,7 @@ export default class Quadtree {
     }
 
     show() {
-        rect(this.x + 1, this.y + 1, this.sizex - 1, this.sizey - 1);
+        rect(this.position.x + 1, this.position.y + 1, this.size.width - 1, this.size.height - 1);
         if (this.hasSplit) this.children.forEach(child => child.show())
     }
 }
